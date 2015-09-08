1c1a22c798cc07b8ed68782fd5680353a3533645
Marco Rojas <Marco1095@outlook.com>
2015-09-08 17:03:42 -0500

    Implementación mejorada

--- LosTresCochinitos.java
+++ LosTresCochinitos.java
@@ -1,23 +1,21 @@
-public class LosTresCochinitos  extends Thread{
-    @Override
-    public void run(){
-    System.out.println("Mi PID es :" + this.getId());
-    //getId() obtiene el PID del hilo del objeto
-}
-    private String nombre;
-    private long pid;
-    LosTresCochinitos(String nombre){
+public class LosTresCochinitos{
+    protected String nombre;
+    public LosTresCochinitos(String nombre)
+    {
         this.nombre = nombre;
-        this.pid = this.getId();
-        System.out.println("Se ha construido el cochino " + this.nombre + ", su PID es: " + this.pid);
     }
-public static void main(String[] args) {
-        LosTresCochinitos cochinos[] = new LosTresCochinitos[3];
-        cochinos[0] = new LosTresCochinitos("Hugo");
-        cochinos[1] = new LosTresCochinitos("Paco");
-        cochinos[2] = new LosTresCochinitos("Luis");
-        cochinos[0].start();
-        cochinos[1].start();
-        cochinos[2].start();
+    public static void main(String[] args) 
+    {
+        LosTresCochinitosDistribuidos cochinos[] = new LosTresCochinitosDistribuidos[3];
+        Thread hilos[] = new Thread[3];
+        //cochinos[0] = new LosTresCochinitosDistribuidos("Hugo");
+        hilos[0] = new Thread(new LosTresCochinitosDistribuidos("Hugo"));
+        //cochinos[1] = new LosTresCochinitosDistribuidos("Paco");
+        hilos[1] = new Thread(new LosTresCochinitosDistribuidos("Paco"));
+        //cochinos[2] = new LosTresCochinitosDistribuidos("Luis");
+        hilos[2] = new Thread(new LosTresCochinitosDistribuidos("Luis"));
+        hilos[0].start();
+        hilos[1].start();
+        hilos[2].start();
     } 
 }
